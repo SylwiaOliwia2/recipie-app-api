ebde5b9f7e37f1e68aac71c6f423d8834be11840
Sylwia <sylwia.kucharska11@gmail.com>
2020-05-25 16:26:08 +0200

    Add Django Admin page

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    # fieldsets makes layout changes in the forms on the “add” and “change” pages such as showing only a subset of available fields
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('presonal_info'), {'fields': ('name',)}),
+        (('Permissions'),{'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin)
